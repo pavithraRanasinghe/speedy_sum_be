d301d845ea2bc775a5df1e56ed40b647f5ff1d44
Pavithra Ranasinghe <paviranasinghe20@gmail.com>
2024-02-02 12:14:22 +0530

    Fetch summarize history

---
+++ src/models/history_response.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+
+class HistoryResponse(BaseModel):
+    data: str
+    type: str
--- src/routers/service_route.py
+++ src/routers/service_route.py
@@ -1,8 +1,7 @@
 from typing import Any
-from urllib import response
 from fastapi import APIRouter, HTTPException, status, Body
 from fastapi.encoders import jsonable_encoder
-from src.models.user_model import User
+from src.models.history_response import HistoryResponse
 from src.models.article import Article
 from src.models.summary_response import SummaryResponse
 from src.models.text_sum_request import TextSumRequest
@@ -19,16 +18,17 @@ router = APIRouter(
     tags=["summarize"]
 )
 
-@router.post("/text", response_description="Summarize", status_code=status.HTTP_200_OK, response_model=SummaryResponse)
+@router.post("/text", response_description="Summarize from text", status_code=status.HTTP_200_OK, response_model=SummaryResponse)
 async def text_summarize(request: TextSumRequest = Body(...)) -> Any:
     keyPhrases = extract_keys(request.text)
     summary = summarize([request.text],keyPhrases, request.min, request.max)
     if(request.user != 0):
-        await save_history(request.user, request.text)
+        await save_history(request.user, request.text, 'TEXT')
            
     return SummaryResponse(summary= summary,keyPhrases= keyPhrases) # type: ignore
 
-@router.post("/link", response_description="Summarize", status_code=status.HTTP_200_OK, response_model=SummaryResponse)
+
+@router.post("/link", response_description="Summarize from link", status_code=status.HTTP_200_OK, response_model=SummaryResponse)
 async def web_page_summarize(request: TextSumRequest = Body(...)) -> Any:
     try:
         articleSet = scrap_data(request.text)
@@ -37,20 +37,25 @@ async def web_page_summarize(request: TextSumRequest = Body(...)) -> Any:
         raise HTTPException(status_code=400, detail='Can not read content of the web page')
     summary = summarize(articleSet, keyPhrases, request.min, request.max)
     if(request.user != 0):
-        await save_history(request.user, request.text)
+        await save_history(request.user, request.text, 'LINK')
     return SummaryResponse(summary= summary,keyPhrases= keyPhrases) # type: ignore
 
-@router.get("/history/{user_id}", response_description="Fetch History", status_code=status.HTTP_200_OK)
-async def find_history(user_id: str):
-    await user_collection.find_one({"id": user_id})
 
-async def save_history(user_id: int, data: str):
+@router.get("/history/{user_id}", response_description="Fetch History", status_code=status.HTTP_200_OK, response_model=HistoryResponse)
+async def find_history(user_id: int):
+    articles = article_collection.find({"user.id": user_id})
+    history_list = []
+    async for a in articles:
+        history_list.append(HistoryResponse(data = a['data'], type = a['type']))
+    return history_list
+
+async def save_history(user_id: int, data: str, type: str):
     user = await user_collection.find_one({"id": user_id})
     if(user):
         user['_id'] = str(user['_id'])
         article = Article(
             data=data,
-            type='TEXT',
+            type=type,
             user= user
         )
         article = jsonable_encoder(article)
